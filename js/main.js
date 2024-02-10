f9e9cc5a1ec85df86d3728a1b05df6857046d80d
bekzoddr <sodiqov.bekzoddr@gmail.com>
2024-02-10 20:14:39 +0500

    clock

--- js/main.js
+++ js/main.js
@@ -40,4 +40,16 @@ galleryItems.forEach(el => {
     });
 });
 
-
+ window.addEventListener("scroll", function () {
+   shrink();
+ });
+ 
+ let navbar = document.getElementById("navbar");
+ 
+ function shrink() {
+   if (scrollY > 100) {
+     navbar.classList.add("navbar-shrink");
+   } else {
+       navbar.classList.remove("navbar-shrink");
+    }
+ }
